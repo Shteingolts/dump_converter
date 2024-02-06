be82660389495ec8defaf18072d68b0e1db0b02a
Sergey <s.shteingolts@gmail.com>
2024-02-06 15:23:53 +0200

    Added README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Dump2Data
+
+## Overview
+A small tool which parses trajectory files produced by LAMMPS and converts them into PyTorch Geometric Data objects.
+
+## Requirements
+- Python 3.10 or higher
+- numpy
+- torch
+- torch_geometric
+
+## Installation
+Clone this repository:
+```sh
+git clone https://github.com/Shteingolts/dump_converter
+```
--- convert.py
+++ convert.py
@@ -8,10 +8,9 @@ the lammps trajectory file format.
 
 Intended as a substite for the helpers.py file in the future.
 """
-from copy import deepcopy
-import numpy as np
 import os
-from sympy import per
+
+import numpy as np
 import torch
 from torch_geometric.data import Data
 
@@ -19,26 +18,26 @@ import network
 
 
 def assemble_data(
-    atoms: list[network.Atom], bonds: list[network.Bond], box: network.Box, node_features: str = "full"
+    atoms: list[network.Atom],
+    bonds: list[network.Bond],
+    box: network.Box,
+    node_features: str = "full",
 ) -> Data:
-    """Helper function, part of the `parse_dump()`. Assembles the pytroch_geometric `Data` object.
-    By default returns only x and y as node features.
-
+    """A helper function which assembles the PyTorch_Geometric `Data` object.
+    
     Parameters
     ----------
-    count : int
-        Index of the current `Data` object
-    atoms : list
-        list of `Atom` objects to become node features
-    bonds : list
-        list of `Bond` objects to become edge features
-    node_features : str, optional
-        what node feature to include, by default "coord"
+    `atoms` : list
+        list of `network.Atom` objects transformed into nodes
+    `bonds` : list
+        list of `network.Bond` objects transformed into edges
+    `node_features` : str, optional
+        which node features to include, by default "coord"
 
     Returns
     -------
     Data
-        pytorch_geometric `Data` object
+        PyTorch_Geometric `Data` object
     """
     # mapping of atomic IDs to their list indices
     atom_ids = torch.tensor([atom.atom_id for atom in atoms])
@@ -54,14 +53,18 @@ def assemble_data(
     ]
 
     edge_index = torch.tensor(np.array(edges_with_indices).T)
-    edge_vectors = [torch.tensor([bond.atom1.x - bond.atom2.x, bond.atom1.y - bond.atom2.y]) for bond in bonds]
+
+    #NOTE: edge vectors will always be defined as vectors between the nodes in the SAME simulation box.
+    # They will be wrond for bonds that cross the simulation box boundary.
+    # On the other hand, edge lengths will always be correct.
+    edge_vectors = [torch.tensor([bond.atom1.x - bond.atom2.x, bond.atom1.y - bond.atom2.y]) for bond in bonds] 
     edge_vectors = torch.stack(edge_vectors)
     edge_lengths = torch.tensor([bond.length for bond in bonds])
     edge_stiffnesses = torch.tensor([bond.bond_coefficient for bond in bonds])
-    
+
     # edge vector and its length
     edge_attr = [
-        torch.cat((v, torch.tensor([length]), torch.tensor([stiffness])))
+        torch.cat((v, torch.tensor([length]), torch.tensor([stiffness]))) 
         for v, length, stiffness in zip(edge_vectors, edge_lengths, edge_stiffnesses)
     ]
     edge_attr = torch.stack(edge_attr)
@@ -102,14 +105,15 @@ def parse_dump(
 
     Parameters
     ----------
-    dump_filepath : str
+    `dump_filepath` : str
         Path to the lammps trajectory files
-    original_network : network.Network
+    `original_network` : network.Network
         simulated network to get the accurate information about periodic bonds
-    node_features : str, optional
-        what node features to parse to parse from trajectory file, by default "full"
-    skip : int, optional
-        load each n-th step starting from the first. 1 means load each step and skip none save time by increasing
+    `node_features` : str, optional
+        node features to include into Data object, by default "full".
+        See `assemble_data()` function for more info.
+    `skip` : int, optional
+        load each n-th step starting from the first, by default 1 (skip none)
 
     Returns
     -------
@@ -125,8 +129,13 @@ def parse_dump(
             timesteps.append(index)
 
     original_network.bonds
-    original_edge_index = [(bond.atom1.atom_id, bond.atom2.atom_id) for bond in original_network.bonds]
-    bond_map = {(bond.atom1.atom_id, bond.atom2.atom_id) : bond for bond in original_network.bonds}
+    original_edge_index = [
+        (bond.atom1.atom_id, bond.atom2.atom_id) for bond in original_network.bonds
+    ]
+    bond_map = {
+        (bond.atom1.atom_id, bond.atom2.atom_id): bond
+        for bond in original_network.bonds
+    }
 
     data_list = []
     for i in range(0, len(timesteps) - 1, skip):
@@ -151,7 +160,11 @@ def parse_dump(
         new_atoms = []
         for atom_data in timestep_data[9:]:
             atom_data = atom_data.split()
-            atom_diameter = [atom for atom in original_network.atoms if atom.atom_id == int(atom_data[0])][0].diameter
+            atom_diameter = [
+                atom
+                for atom in original_network.atoms
+                if atom.atom_id == int(atom_data[0])
+            ][0].diameter
             # print(atom_diameter)
             atom = network.Atom(
                 atom_id=int(atom_data[0]),
@@ -165,15 +178,16 @@ def parse_dump(
             atom.vz = float(atom_data[6])
             new_atoms.append(atom)
 
-        new_atom_map = {atom.atom_id : atom for atom in new_atoms}
+        new_atom_map = {atom.atom_id: atom for atom in new_atoms}
         new_bonds = []
         for edge_index in original_edge_index:
             id1, id2 = edge_index[0], edge_index[1]
             new_atom1 = new_atom_map[id1]
             new_atom2 = new_atom_map[id2]
-            old_stiffness = bond_map[(id1, id2)].bond_coefficient
-            
-            # ensure that the two atoms are located close to each other
+            bond_stiffness = bond_map[(id1, id2)].bond_coefficient
+
+            # calculate the proper distance between two bonded atoms 
+            # keeping periodicity in mind
             if abs(new_atom1.x - new_atom2.x) > original_network.box.x / 2:
                 real_x2 = max(new_atom1.x, new_atom2.x)
                 real_x1 = min(new_atom1.x, new_atom2.x) + original_network.box.x
@@ -186,13 +200,13 @@ def parse_dump(
             else:
                 real_y1 = new_atom1.y
                 real_y2 = new_atom2.y
-            
-            new_length = ((real_x2-real_x1)**2 + (real_y2-real_y1)**2)**0.5
+
+            new_length = ((real_x2 - real_x1) ** 2 + (real_y2 - real_y1) ** 2) ** 0.5
             new_bond = network.Bond(new_atom1, new_atom2)
             new_bond.length = new_length
-            new_bond.bond_coefficient = old_stiffness
+            new_bond.bond_coefficient = bond_stiffness
             new_bonds.append(new_bond)
-            
+
         data_list.append(assemble_data(new_atoms, new_bonds, box, node_features=node_features))
 
     return data_list
@@ -213,30 +227,25 @@ def bulk_load(
     -------
     list[list[Data]]
     """
-    network_sims = [
-        os.path.abspath(os.path.join(data_dir, directory, "sim"))
+    sim_dirs = [
+        os.path.abspath(os.path.join(data_dir, directory))
         for directory in os.listdir(data_dir)
     ]
-
     data = []
-    for index, sim in enumerate(network_sims):
+    for index, sim_dir in enumerate(sim_dirs):
         # reading network from `coord.dat` instead of `*.lmp` to get the accurate information about periodic bonds
         current_network = network.Network.from_atoms(
-            os.path.join(sim, "../", "coord.dat"),
+            os.path.join(sim_dir, "coord.dat"),
             include_angles=False,
             include_dihedrals=False,
         )
-        current_network.write_to_file(os.path.join(sim, "true_network.lmp"))
-        dump_file = os.path.join(sim, "dump.lammpstrj")
-        print(f"{index+1}/{len(network_sims)} : {dump_file}")
-        data.append(
-            parse_dump(
-                dump_file, current_network, node_features=node_features, skip=skip
-            )
-        )
+        current_network.write_to_file(os.path.join(sim_dir, "true_network.lmp"))
+        dump_file = os.path.join(sim_dir, "dump.lammpstrj")
+        print(f"{index+1}/{len(sim_dirs)} : {dump_file}")
+        data.append(parse_dump(dump_file, current_network, node_features=node_features, skip=skip))
 
         # stop loading of desired number of network simulation is parsed
-        if index+1 >= n_networks:
+        if index + 1 >= n_networks:
             break
     return data
 
@@ -247,11 +256,11 @@ def dump(data: list[Data], filedir: str = "", filename: str = "dump_custom.lammp
 
     Parameters
     ----------
-    data : list[Data]
+    `data` : list[Data]
         list of Data objects to dump into a file as trajectory
-    filedir : str, optional
+    `filedir` : str, optional
         directory to write the output file to, by default ""
-    filename : str, optional
+    `filename` : str, optional
         output file name, by default "dump_custom.lammpstrj"
     """
     filepath = os.path.join(filedir, filename)
@@ -280,8 +289,4 @@ def bulk_dump(data_dir: str):
 
 
 if __name__ == "__main__":
-    example = network.Network.from_data_file(
-        "true_network.lmp", include_angles=False, include_dihedrals=False
-    )
-    data = parse_dump("dump.lammpstrj", example)
-    dump(data)
+    pass
